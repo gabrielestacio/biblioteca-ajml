a74c650e3fbe32f235f12496a8bb1238f615d046
Herbet Pessoa da Silva Filho <98792986+Herbet-Pessoa@users.noreply.github.com>
2022-12-12 13:34:58 -0300

    jobs

--- biblioteca-master/src/main/java/edu/uepb/web/biblioteca/jobs/EnviaEmailReserva.java
+++ biblioteca-master/src/main/java/edu/uepb/web/biblioteca/jobs/EnviaEmailReserva.java
@@ -8,8 +8,8 @@ import org.quartz.JobExecutionException;
  * @autor geovanniovinhas <vinhasgeovannio@gmail.com
  */
 public class EnviaEmailReserva implements Job{
-	public static final String EMAIL = "email";
-	public static final String DATA_PEGAR = "dataPegar";
+	public static final/*@ spec_public non_null @*/ String EMAIL = "email";
+	public static final /*@ spec_public non_null @*/String DATA_PEGAR = "dataPegar";
 	
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
